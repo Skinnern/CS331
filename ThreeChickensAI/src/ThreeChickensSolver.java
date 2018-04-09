b3380cacc137e197b138a54d8f1f13de82333ef5
Skinner <Nick.Skinner@dnvgl.com>
2018-04-09 13:22:55 -0700

    Finish String to Int Array

--- ThreeChickensAI/src/ThreeChickensSolver.java
+++ ThreeChickensAI/src/ThreeChickensSolver.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * Created by nicks on 4/6/2018.
@@ -7,21 +8,50 @@ public class ThreeChickensSolver {
     public static void main(String[] args){
         //System.out.println("Hello World" + args[0] + args[1]+ args[2] + args[3]);
 
-        //inital array
+        int[] initialInt = new int[6];
+        int[] goalInt = new int[6];
+
+        //initial array
+        initialInt = ConvertDriver(args[0]);
+
+        int i=0;
+        while(i<initialInt.length) {
+            System.out.println(initialInt[i]);
+            i++;
+        }
 
         //goal array
+        goalInt = ConvertDriver(args[1]);
 
-
+        i=0;
+        while(i<goalInt.length) {
+            System.out.println(goalInt[i]);
+            i++;
+        }
         //read testStart1.txt into java
+
+        //read testGoal1.txt into java
+
+        //mode setter
+
+        //file output
+
+    }
+
+    public static String[] fileRead(String fileName){
+
+        String[] Arr = new String[6];
+
         try {
-            FileReader fr = new FileReader(args[0]);
-            System.out.println("Grabbed file!");
+            FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
 
             //print contents
             String str;
+            int i=0;
             while((str = br.readLine()) != null){
-                System.out.println(str + "\n");
+                Arr[i] = str;
+                i++;
             }
             br.close();
         }
@@ -29,38 +59,42 @@ public class ThreeChickensSolver {
         catch(IOException e){
             System.out.println("Error: \n\n" + e);
         }
+        return Arr;
+    }
 
+    public static String[] stringSplit(String[] toSplit){
+        String[] splitArr = new String[6];
+        String[] splitArrStr = toSplit[0].split(",");
+        String[] splitArrStr2 = toSplit[1].split(",");
 
-        //read testGoal1.txt into java
-        try {
-            FileReader fr2 = new FileReader(args[1]);
-            System.out.println("Grabbed file!");
-            BufferedReader br2 = new BufferedReader(fr2);
+        splitArr[0] = splitArrStr[0];
+        splitArr[1] = splitArrStr[1];
+        splitArr[2] = splitArrStr[2];
+        splitArr[3] = splitArrStr2[0];
+        splitArr[4] = splitArrStr2[1];
+        splitArr[5] = splitArrStr2[2];
 
-            //print contents
-            String str2;
-            while((str2 = br2.readLine()) != null){
-                System.out.println(str2 + "\n");
+        return splitArr;
+    }
 
-            }
-            br2.close();
-        }
-        //catch error from testGoal1.txt reading
-        catch(IOException e){
-            System.out.println("Error: \n\n" + e);
-        }
+    public static int[] turnToInt(String strToTransform[]){
+    int[] intArr=new int[6];
+    int i=0;
+    while(i<strToTransform.length){
+        intArr[i] = Integer.parseInt(strToTransform[i]);
+        i++;
+    }
 
+    return intArr;
+    }
+    public static int[] ConvertDriver(String fileToRead){
+        String[] inputStr = new String[6];
+        int[] inputInt = new int[6];
 
-        //mode setter
+        inputStr = fileRead(fileToRead);
+        inputStr = stringSplit(inputStr);
+        inputInt = turnToInt(inputStr);
 
-
-
-
-        //file output
-
-
-
-
-
+        return inputInt;
     }
 }
